a4683649f6caa904d948f435fc978d5cfcf177c1
Anton Krasovski <antohkad21@gmail.com>
2024-01-15 20:27:45 +0300

    Add task: take a ten minutes walk

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,24 @@
 "use strict"
+
+//Task: Take a ten minutes walk
+function isValidWalk (walk) {
+    let dx = 0;
+    let dy = 0;
+    const min = walk.length;
+
+    walk.forEach(el => {
+        switch (el) {
+            case 'n': dy++; break
+            case 's': dy--; break
+            case 'w': dx++; break
+            case 'e': dx--; break
+        }
+    })
+
+    return min === 10 && dy === 0 && dx === 0
+}
+
+console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s']))
+console.log(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']))
+console.log(isValidWalk(['n','n','n','s','n','s','n','s','n','s']))
+console.log(isValidWalk(['w']))
