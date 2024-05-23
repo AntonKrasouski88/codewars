e1336c99fdd45a32821feebf21041322624df356
Anton Krasovski <antohkad21@gmail.com>
2024-05-23 21:34:45 +0300

    Add the solution to the task 'If you can't sleep, just count sheep'

--- python/tasks.py
+++ python/tasks.py
@@ -11,3 +11,17 @@ def square_digits(num):
 
 print(square_digits(9119))
 print(square_digits(0))
+
+
+#Task: If you can't sleep, just count sheep!!
+def count_sheep(n):
+    str =''
+    for i in range(1, n + 1):
+        str += f'{i} sheep...'
+    return str
+
+print(count_sheep(3))
+print(count_sheep(0))
+
+#def count_sheep(n):
+    #return ''.join(f"{i} sheep..." for i in range(1,n+1))
