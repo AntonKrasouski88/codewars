295ab1a4a49da57979d10fc296cca80974939246
Anton Krasovski <antohkad21@gmail.com>
2024-05-28 22:34:57 +0300

    Add the solution to the task(js) 'CSV representation of array'

--- javascript/index.js
+++ javascript/index.js
@@ -336,3 +336,16 @@ function sumMix(arr) {
 }
 
 console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']));
+
+
+//Task: CSV representation of array
+function toCsvText(array) {
+    return array.join('/n')
+}
+
+console.log(toCsvText([
+    [ 0, 1, 2, 3, 45 ],
+    [ 10,11,12,13,14 ],
+    [ 20,21,22,23,24 ],
+    [ 30,31,32,33,34 ]
+   ] ));
