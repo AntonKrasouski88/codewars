34407e8dbc35786269db826263d9b2c815386588
Anton Krasovski <antohkad21@gmail.com>
2024-06-02 10:43:28 +0300

    Add the solution to the task(js) 'Small enough? - beginner'

--- javascript/index.js
+++ javascript/index.js
@@ -439,3 +439,12 @@ function cappitals(word) {
 }
 
 console.log(cappitals('CodEWaRs'));
+
+
+//Task: Small enough? - Beginner
+function smallEnough(a, limit) {
+    return Math.max(...a) <= limit;
+}
+
+console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100));
+console.log(smallEnough([101, 45, 75, 105, 99, 107], 107));
