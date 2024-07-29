816b593327b59a6ef64f8d3bec95cb8d0dead896
Anton Krasovski <antohkad21@gmail.com>
2024-07-29 21:54:36 +0300

    Add: a solution to the task(python) 'Disemvowel Trolls'

--- python/tasks.py
+++ python/tasks.py
@@ -90,3 +90,14 @@ def get_count(sentence):
 
 print(get_count("aeiou"))
 print(get_count("bcdfghjklmnpqrstvwxz y"))
+
+
+# Task: Disemvowel Trolls
+def disemvowel(string_):
+    
+    for char in 'aeiouAEIOU':
+        string_ = string_.replace(char, '')
+
+    return string_
+
+print(disemvowel("This website is for losers LOL!"))
