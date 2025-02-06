461d0f9f75a0934952997cd39fa1269b07f20a40
Anton Krasovki <antohkad21@gmail.com>
2025-02-06 21:22:18 +0300

    Add: a solution for the task 'Contamination#1 string'

--- javascript/index.js
+++ javascript/index.js
@@ -1136,3 +1136,9 @@ function tram(stops, descending, onboarding) {
 console.log(tram(4, [0, 2, 4, 4], [3, 5, 2, 0]));
 console.log(tram(2, [0, 2, 4, 4], [3, 5, 2, 0]));
 console.log(tram(10, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]));
+
+
+//Task: Contamination#1 String
+function contamination(text, char) {
+    return char.repeat(text.length)
+}
