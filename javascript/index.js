5dbdeb252b96b6c5c1900c11e5b04576756d8dee
Anton Krasovki <antohkad21@gmail.com>
2025-03-12 22:26:44 +0300

    add solution for the task(js) 'USD=>CNY'

--- javascript/index.js
+++ javascript/index.js
@@ -1290,3 +1290,7 @@ function capMe(names) {
 }
 
 console.log(capMe(["jo", "nelson", "jurie"]));
+
+//USD=>CNY
+const usdcny = (usd) => `${(usd * 6.75).toFixed(2)} Chinese Yuan`;
+console.log(usdcny(6840));
