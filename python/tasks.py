dfe70358e7070c90b468ea4c50744fae984ed632
Anton Krasovski <antohkad21@gmail.com>
2025-05-24 21:09:26 +0300

    add a solution for the task(python): 'Create phone number'

--- python/tasks.py
+++ python/tasks.py
@@ -796,3 +796,13 @@ def remove_char(s):
     return s[1:-1]
 
 print(remove_char("hello"))
+
+
+# Create phone number
+def create_phone_number(n):
+    # number = ''.join([str(el) for el in n ])
+    # return f'({(number[0:3])}) {number[3:6]}-{number[6:]}'
+    return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
+
+
+print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
