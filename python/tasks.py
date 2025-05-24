132c68ee6bffe65d288d75bf6a57e50e455554cd
Anton Krasovski <antohkad21@gmail.com>
2025-05-24 20:34:20 +0300

    add a solution for the task(python) 'Remove first and last char'

--- python/tasks.py
+++ python/tasks.py
@@ -789,3 +789,10 @@ def maskify(cc):
     
 print(maskify('SF$SDfgsd2eA'))
 print(maskify('SF'))
+
+
+# Remove First and Last Character
+def remove_char(s):
+    return s[1:-1]
+
+print(remove_char("hello"))
