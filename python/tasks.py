c7825eaeca0c30fb95e45cd1d36c422b32327046
Anton Krasovski <antohkad21@gmail.com>
2025-06-23 18:29:48 +0300

    Add a solution for the task(python) 'Classification of segments'

--- python/tasks.py
+++ python/tasks.py
@@ -982,3 +982,24 @@ def is_happy_ticket(series):
 
 print(is_happy_ticket('123123')) # True
 print(is_happy_ticket('341801')) # False
+
+
+# Classification of segments
+line1 = ((5, 1), (20, 10))
+
+def is_degenerated(line):
+    return line[0][0] == line[1][0] and line[0][1] == line[1][1]
+
+def is_horizontal(line):
+    return line[0][0] != line[1][0] and line[0][1] == line[1][1]
+
+def is_vertical(line):
+    return line[0][0] == line[1][0] and line[0][1] != line[1][1]
+
+def is_inclined(line):
+    return line[0][0] != line[1][0] and line[0][1] != line[1][1]
+
+print(is_degenerated(line1))
+print(is_horizontal(line1))
+print(is_vertical(line1))
+print(is_inclined(line1))
