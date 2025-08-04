153d234a42c314b3707dfa86716b49c20c9ae348
Anton Krasovki <antohkad21@gmail.com>
2025-08-04 20:35:15 +0300

    Add a solution for the task 'Smallest divisors'

--- javascript/index.js
+++ javascript/index.js
@@ -1803,3 +1803,24 @@ console.log(sequenceSum(7, 2));
 console.log(sequenceSum(0, 0));
 console.log(sequenceSum(4, 10));
 console.log(sequenceSum(-3, 2));
+
+// Smallest divisors
+const smallestDivisors = (num) => {
+  const iter = (acc) => {
+    // We use 'num / 2' in the condition below, and not 'num'.
+    // This is a simple optimization: a number cannot be divided
+    // by a number larger than its half.
+    if (acc > num / 2) {
+      return num;
+    }
+    if (num % acc === 0) {
+      return acc;
+    }
+    return iter(acc + 1);
+  };
+
+  return iter(2);
+};
+
+console.log(smallestDivisors(15));
+console.log(smallestDivisors(3));
