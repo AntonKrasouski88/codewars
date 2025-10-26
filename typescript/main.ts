bf8a9ca09763651e590193675f77e95a79046c65
AntonKrasovki88 <antohkad21@gmail.com>
2025-10-26 14:02:38 +0300

    Add a solution for the task (typescript) 'Grasshoper  -  Basic Function Fixer'

---
+++ typescript/main.ts
@@ -0,0 +1,11 @@
+function toGreet(str: string): string {
+  return `Hello, ${str}`;
+}
+
+console.log(toGreet("World"));
+
+// Grasshoper - Basic Function Fixer
+const addFive = (num: number): number => {
+  let total = num + 5;
+  return total;
+};
