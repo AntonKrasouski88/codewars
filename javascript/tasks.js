225869cb882722bd3b9aaecfaab7d0a776da226a
Anton Krasovki <antohkad21@gmail.com>
2026-02-07 20:07:53 +0300

    feat: add a solution for the task: 'Are the numbers in order'

--- javascript/tasks.js
+++ javascript/tasks.js
@@ -10,3 +10,24 @@ function multiplies(m, n) {
 
 console.log(multiplies(3, 5));
 console.log(multiplies(5, -1));
+
+//Are the numbers in order
+function inAscOrder(arr) {
+  const len = arr.length;
+  if (len === 0 || len === 1) {
+    return true;
+  }
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[i - 1]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(inAscOrder([1, 2, 4, 7, 19]));
+console.log(inAscOrder([1, 6, 10, 18, 2, 4, 20]));
+console.log(inAscOrder([1]));
+console.log(inAscOrder([]));
